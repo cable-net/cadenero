e9144242e9bd6d3c56da79ee0026d2b6bfd09262
betuzo <betotsol@gmail.com>
2022-07-14 01:19:04 -0500

    agregando prueba de fallo

--- integration/auth.test.js
+++ integration/auth.test.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const app = require('../app')
 const Usuario = require('../models/usuario')
 
-describe('api/users', () => {
+describe('Pruebas para la autenticacion en la plataforma', () => {
   before(async () => {
     // before each test delete all users table data
     await Usuario.deleteMany({})
@@ -16,8 +16,8 @@ describe('api/users', () => {
     mongoose.disconnect()
   })
 
-  describe('POST /', () => {
-    it('should return user when the all request body is valid', async () => {
+  describe('Endpoint de registro', () => {
+    it('deberia retornar un usuario con un id y una fecha de registro en la BD', async () => {
       const usuario = {
         email: 'a45srgh@gmail.com',
         password: '1q2w3e4r',
@@ -28,6 +28,19 @@ describe('api/users', () => {
       const res = await request(app).post('/api/auth/register').send(usuario)
       expect(res.status).to.equal(201)
       expect(res.body).to.have.include.keys('_id', 'date')
+    })
+
+    it('deberia retornar un error porque el email ya existe', async () => {
+      const usuario = {
+        email: 'a45srgh@gmail.com',
+        password: '1q2w3e4r',
+        user_id: '62ce8f01c62c6a277bba2e31',
+        user_type: 'cliente',
+        role: 'SUPERVISOR'
+      }
+      const res = await request(app).post('/api/auth/register').send(usuario)
+      expect(res.status).to.equal(400)
+      expect(res.body.error).to.equal('Verifique sus datos')
     })
   })
 })
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,13 @@ const Usuario = require('../models/usuario')
 const bcrypt = require('bcrypt')
 
 router.post('/register', async (req, res) => {
+  const isEmailExist = await Usuario.findOne({ email: req.body.email });
+  if (isEmailExist) {
+    return res.status(400).json(
+        {error: 'Verifique sus datos'}
+    )
+  }
+
   const salt = await bcrypt.genSalt(10)
   const passwordHash = await bcrypt.hash(req.body.password, salt)
   const usuario = new Usuario({
@@ -15,9 +22,7 @@ router.post('/register', async (req, res) => {
   })
   try {
     const savedUsuario = await usuario.save()
-    res
-      .status(201)
-      .json(savedUsuario)
+    res.status(201).json(savedUsuario)
   } catch (error) {
     res.status(400).json({ error })
   }

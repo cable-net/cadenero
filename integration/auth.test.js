19a61b3198e49872a43d5986340c368949f9ffdd
tahed1617 <101617512+tahed1617@users.noreply.github.com>
2022-07-14 23:36:29 -0500

    login endpoint y pruebas terminadas

--- integration/auth.test.js
+++ integration/auth.test.js
@@ -32,12 +32,13 @@ describe('Pruebas para la autenticacion en la plataforma', () => {
 
     it('deberia retornar un error porque el email ya existe', async () => {
       const usuario = {
-        email: 'a45srgh@gmail.com',
+        email: 'prueba78@gmail.com',
         password: '1q2w3e4r',
         user_id: '62ce8f01c62c6a277bba2e31',
         user_type: 'cliente',
         role: 'SUPERVISOR'
       }
+      await request(app).post('/api/auth/register').send(usuario)
       const res = await request(app).post('/api/auth/register').send(usuario)
       expect(res.status).to.equal(400)
       expect(res.body.error).to.equal('Verifique sus datos')
@@ -67,6 +68,64 @@ describe('Pruebas para la autenticacion en la plataforma', () => {
       const res = await request(app).post('/api/auth/register').send(usuario)
       expect(res.status).to.equal(400)
       expect(res.body.error).to.equal('Datos invalidos')
+    })
+  })
+
+  describe('Endpoint de login', () => {
+    it('deberia retornar el token del usuario', async () => {
+      const register = {
+        email: 'pruebalogin@gmail.com',
+        password: '1q2w3e4r',
+        user_id: '62ce8f01c62c6a277bba2e31',
+        user_type: 'cliente',
+        role: 'SUPERVISOR'
+      }
+      await request(app).post('/api/auth/register').send(register)
+      const usuario = {
+        email: 'pruebalogin@gmail.com',
+        password: '1q2w3e4r'
+      }
+      const res = await request(app).post('/api/auth/login').send(usuario)
+      expect(res.status).to.equal(200)
+      expect(res.body).to.have.include.keys('token')
+    })
+
+    it('deberia retornar un error si el password es muy pequeno', async () => {
+      const usuario = {
+        email: 'a457777@gmail.com',
+        password: '1q2w'
+      }
+      const res = await request(app).post('/api/auth/login').send(usuario)
+      expect(res.status).to.equal(400)
+      expect(res.body.error).to.equal('Verifique sus datos')
+    })
+
+    it('deberia retornar un error si el email no existe', async () => {
+      const usuario = {
+        email: 'otro@gmai.com',
+        password: '1q2w56a'
+      }
+      const res = await request(app).post('/api/auth/login').send(usuario)
+      expect(res.status).to.equal(400)
+      expect(res.body.error).to.equal('Verifique sus datos')
+    })
+
+    it('deberia retornar un error porque el usuario existe pero el password es incorrecto', async () => {
+      const register = {
+        email: 'pruebafail@gmail.com',
+        password: 'phfhru234',
+        user_id: '62ce8f01c62c6a277bba2e31',
+        user_type: 'cliente',
+        role: 'SUPERVISOR'
+      }
+      await request(app).post('/api/auth/register').send(register)
+      const usuario = {
+        email: 'pruebafail@gmail.com',
+        password: '1q2w56a'
+      }
+      const res = await request(app).post('/api/auth/login').send(usuario)
+      expect(res.status).to.equal(400)
+      expect(res.body.error).to.equal('Verifique sus datos')
     })
   })
 })

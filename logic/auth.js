e0e8731487f82fe0f3a9d79b7f36d4d040a259af
betuzo <betotsol@gmail.com>
2022-09-07 10:05:22 -0500

    logic to compute the expired for token

--- logic/auth.js
+++ logic/auth.js
@@ -1,4 +1,3 @@
-module.exports.calculatedExpiresIn = function (expiresInHrs) {
-  const d = new Date()
+module.exports.calculatedExpiresIn = function (d, expiresInHrs) {
   return (((d.getTime()) + (expiresInHrs * 60 * 60 * 1000)) - (d.getTime() - d.getMilliseconds()) / 1000)
 }
